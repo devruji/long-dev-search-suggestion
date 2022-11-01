197b00c5795c9d38336df15dadd88b61082418bb
Rujikorn Ngoensaard <en.rujikorn@gmail.com>
2022-11-01 19:06:08 +0700

    update column mapping

--- app/models/user.py
+++ app/models/user.py
@@ -3,11 +3,11 @@ from app.extensions import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(200), unique=True)
-    website = db.Column(db.String(200), unique=True)
+    keyword = db.Column(db.String(200), unique=True)
+    score = db.Column(db.Float)
 
     def __str__(self):
-        return f"Person ID is {self.name} | Name is {self.name} | Website is {self.website}"
+        return f"ID is {self.id} | Keyword is {self.Keyword} | Score is {self.score}"
 
     def __repr__(self):
-        return f"Person(id={self.id}, name={self.name}, website={self.website})"
+        return f"Users(id={self.id}, keyword={self.keyword}, score={self.score})"
