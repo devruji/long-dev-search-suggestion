ee519fc98696e3daa34760120c42dde8bf4a58d1
Rujikorn Ngoensaard <en.rujikorn@gmail.com>
2022-11-01 19:04:49 +0700

    add db migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
-from app.extensions import db
+from app.extensions import db, migrate
 
 
 # ?: Application Factory
@@ -39,8 +39,9 @@ def cls_config_selector():
 
 
 def install_extensions(app):
-    db.init_app(app)
     csrf.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 
 def register_blueprints(app):
